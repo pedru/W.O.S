39658681de2719c681350fc5f05c8f2c0a6b2a16
Anja Reibenbacher <anja.reibenbacher@student.tugraz.at>
2018-04-17 12:54:36 +0200

    [AR, TP] Tests for calender in search exam

--- Lea/app/src/androidTest/java/wos/lea/SearchExamTest.java
+++ Lea/app/src/androidTest/java/wos/lea/SearchExamTest.java
@@ -17,6 +17,10 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.core.AllOf.allOf;
+import android.support.test.espresso.contrib.PickerActions;
 
 @RunWith(AndroidJUnit4.class)
 public class SearchExamTest extends ActivityInstrumentationTestCase2<SearchExamActivity> {
@@ -41,7 +45,17 @@ public class SearchExamTest extends ActivityInstrumentationTestCase2<SearchExamA
         onView(withId(R.id.studyProgramSpinner)).check(matches(isDisplayed()));
         onView(withId(R.id.courseSpinner)).check(matches(isDisplayed()));
         onView(withId(R.id.dateButton)).check(matches(isClickable()));
+        //onView(withId(R.id.dateButton)).perform(click());
+        //onView(withId(R.id.calenderSearch)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void CalenderTest() {
+        onView(withId(R.id.calenderSearch)).check(matches(not(isDisplayed())));
         onView(withId(R.id.dateButton)).perform(click());
         onView(withId(R.id.calenderSearch)).check(matches(isDisplayed()));
+        onView(withId(R.id.calenderSearch)).perform(PickerActions.setDate(2018, 6, 30));
+        onView(withId(R.id.calenderSearch)).check(matches(not(isDisplayed())));
+        onView(allOf(withId(R.id.dateButton), withText("30.06.2018")));
     }
 }
