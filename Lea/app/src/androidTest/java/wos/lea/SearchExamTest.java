5802f9434ed8c85a0806a27b05c0cdec4e871e97
tinabambina <tina.promitzer@student.tugraz.at>
2018-04-17 14:02:40 +0200

    [AR, TP] implemented datepicker dialog

--- Lea/app/src/androidTest/java/wos/lea/SearchExamTest.java
+++ Lea/app/src/androidTest/java/wos/lea/SearchExamTest.java
@@ -7,20 +7,24 @@ import android.test.ActivityInstrumentationTestCase2;
 
 import junit.framework.Assert;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.core.AllOf.allOf;
 import android.support.test.espresso.contrib.PickerActions;
+import android.widget.DatePicker;
 
 @RunWith(AndroidJUnit4.class)
 public class SearchExamTest extends ActivityInstrumentationTestCase2<SearchExamActivity> {
@@ -44,18 +48,16 @@ public class SearchExamTest extends ActivityInstrumentationTestCase2<SearchExamA
         onView(withId(R.id.dateSearch)).check(matches(isDisplayed()));
         onView(withId(R.id.studyProgramSpinner)).check(matches(isDisplayed()));
         onView(withId(R.id.courseSpinner)).check(matches(isDisplayed()));
-        onView(withId(R.id.dateButton)).check(matches(isClickable()));
-        //onView(withId(R.id.dateButton)).perform(click());
-        //onView(withId(R.id.calenderSearch)).check(matches(isDisplayed()));
     }
 
     @Test
     public void CalenderTest() {
-        onView(withId(R.id.calenderSearch)).check(matches(not(isDisplayed())));
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).check(doesNotExist());
         onView(withId(R.id.dateButton)).perform(click());
-        onView(withId(R.id.calenderSearch)).check(matches(isDisplayed()));
-        onView(withId(R.id.calenderSearch)).perform(PickerActions.setDate(2018, 6, 30));
-        onView(withId(R.id.calenderSearch)).check(matches(not(isDisplayed())));
-        onView(allOf(withId(R.id.dateButton), withText("30.06.2018")));
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).check(matches(isDisplayed()));
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2018, 8, 25));
+        onView(withText("OK")).perform(click());
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).check(doesNotExist());
+        onView(allOf(withId(R.id.dateButton), withText("25.8.2018")));
     }
 }
--- Lea/app/src/main/java/wos/lea/SearchExamActivity.java
+++ Lea/app/src/main/java/wos/lea/SearchExamActivity.java
@@ -1,14 +1,44 @@
 package wos.lea;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 
 public class SearchExamActivity extends AppCompatActivity {
-
+Button showCalenderButton;
+int d,m,y;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_exam);
+        findcontrol();
+        showCalenderButton.setText("Select Date");
+        showCalenderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                d = calendar.get(Calendar.DAY_OF_MONTH);
+                m = calendar.get(Calendar.MONTH);
+                y = calendar.get(Calendar.YEAR);
+                DatePickerDialog pickerDialog = new DatePickerDialog(SearchExamActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        i1+=1;
+                        showCalenderButton.setText(i2 + "." + i1 + "." + i);
+                    }
+                }, y, m, d);
+                pickerDialog.show();
+            }
+        });
+    }
+
+    private void findcontrol() {
+        showCalenderButton=findViewById(R.id.dateButton);
     }
 }
