671dd1297196ecc89c92018f3ac14dfca49de03d
tinabambina <tina.promitzer@student.tugraz.at>
2018-04-18 15:00:41 +0200

    [AR, TP] implemented functionality of dropdowns in Search Exam Activity and added Tests

--- Lea/app/src/androidTest/java/wos/lea/SearchExamTest.java
+++ Lea/app/src/androidTest/java/wos/lea/SearchExamTest.java
@@ -12,6 +12,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
@@ -20,7 +21,10 @@ import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.core.AllOf.allOf;
 import android.support.test.espresso.contrib.PickerActions;
@@ -59,5 +63,16 @@ public class SearchExamTest extends ActivityInstrumentationTestCase2<SearchExamA
         onView(withText("OK")).perform(click());
         onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).check(doesNotExist());
         onView(allOf(withId(R.id.dateButton), withText("25.8.2018")));
+    }
+
+    @Test
+    public void SpinnerTest() {
+        onView(withId(R.id.studyProgramSpinner)).perform(click());
+        onData(anything()).atPosition(1).perform(click());
+        onView(withId(R.id.studyProgramSpinner)).check(matches(withSpinnerText(containsString("Business Services"))));
+
+        onView(withId(R.id.courseSpinner)).perform(click());
+        onData(anything()).atPosition(2).perform(click());
+        onView(withId(R.id.courseSpinner)).check(matches(withSpinnerText(containsString("Computers"))));
     }
 }
--- Lea/app/src/main/java/wos/lea/SearchExamActivity.java
+++ Lea/app/src/main/java/wos/lea/SearchExamActivity.java
@@ -4,20 +4,26 @@ import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Spinner;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class SearchExamActivity extends AppCompatActivity {
-Button showCalenderButton;
-int d,m,y;
+    Button showCalenderButton;
+    Spinner studyProgramSpinner;
+    Spinner courseSpinner;
+    int d,m,y;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_exam);
-        findcontrol();
+        findControlls();
         showCalenderButton.setText("Select Date");
         showCalenderButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,9 +42,34 @@ int d,m,y;
                 pickerDialog.show();
             }
         });
+        SetDropdownElements();
     }
 
-    private void findcontrol() {
-        showCalenderButton=findViewById(R.id.dateButton);
+    private void findControlls() {
+        showCalenderButton = findViewById(R.id.dateButton);
+        studyProgramSpinner = findViewById(R.id.studyProgramSpinner);
+        courseSpinner = findViewById(R.id.courseSpinner);
+    }
+
+    private void SetDropdownElements() {
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("Automobile");
+        categories.add("Business Services");
+        categories.add("Computers");
+        categories.add("Education");
+        categories.add("Personal");
+        categories.add("Travel");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        studyProgramSpinner.setAdapter(dataAdapter);
+        courseSpinner.setAdapter(dataAdapter);
     }
 }
