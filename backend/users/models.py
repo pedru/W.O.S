006643686783a5750e5f198eadccb1aeae75396a
Peter Jochum <peter.jochum@aau.at>
2018-04-19 14:58:41 +0200

    [PJ,OT] Enabled database migration in backend

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,10 +1,1 @@
 from django.db import models
-
-# Create your models here.
-
-
-class BackendUser(models.Model):
-    """
-    Empty user object
-    """
-    token = models.CharField(max_length=32, help_text="Token is the authentication for a user")
