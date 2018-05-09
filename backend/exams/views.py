3b3f3b6b18bc9b7265d4f42eff7139673c58b7cd
Peter Jochum <peter.jochum@aau.at>
2018-05-09 15:42:37 +0200

    [PJ,MH] Added request logging

--- backend/exams/views.py
+++ backend/exams/views.py
@@ -3,11 +3,14 @@ from rest_framework import viewsets, generics
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+import logging
 
 from exams.models import Exam, Question, Lecture
 from exams.serializers import ExamListSerializer, QuestionListSerializer, LectureDetailSerializer, LectureSerializer, \
     ExamDetailSerializer
 
+
+logger = logging.getLogger(__name__)
 
 class ExamViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Exam.objects.all()
