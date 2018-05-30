b88df3fd17e21aea888b057fc2ca405c6282fe30
Peter Jochum <peter.jochum@aau.at>
2018-05-30 16:00:34 +0200

    [PJ,PE] Fixes for framework documentation (automatic)

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,20 +1,5 @@
-"""backend URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, reverse_lazy
+from django.urls import path
 from django.conf.urls import url, include
 from django.views.generic import RedirectView
 from rest_framework import routers
@@ -30,6 +15,8 @@ router.register(r'exams', ExamViewSet)
 router.register(r'studies', StudyListViewSet)
 router.register(r'exams', ExamViewSet)
 router.register(r'lecture', LectureViewSet)
+router.register(r'questions', QuestionViewSet)
+router.register(r'answers', AnswerViewSet)
 
 
 admin.site.site_header = 'LeaBackend'
@@ -38,7 +25,6 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^api/', include(router.urls)),
     url(r'^docs/', include_docs_urls(title='Lea Rest-API', public=False)),
-    url(r'^$', schema_view),
     path('admin/', admin.site.urls),
     #url('^$', RedirectView.as_view(url=reverse_lazy('admin:index'))),
     url('^api/user/token', create_user),
